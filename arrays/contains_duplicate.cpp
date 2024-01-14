241f3cff1757b1d78ac0bb687a4c68457b9590e3
saboorbakshi <saboorbak@icloud.com>
2024-01-14 17:48:07 -0500

    commented code

--- arrays/contains_duplicate.cpp
+++ arrays/contains_duplicate.cpp
@@ -4,11 +4,17 @@ using namespace std;
 
 class Solution {
 public:
+
+    /* 
+    insert each item in an unordered set until you come across a previously inserted element
+    time: O(n)
+    space: O(n)
+    */
     bool containsDuplicate1(vector<int>& nums) {
         set<int> myset;
 
         for (auto n : nums) {
-            if (myset.find(n) != myset.end()) {
+            if (myset.find(n) != myset.end()) {   // find() returns set::end if element not found
                 return true;
             };
             myset.insert(n);
@@ -16,18 +22,23 @@ public:
         return false;
     }
 
+    /*
+    sort the array, check if i and i+1 are the same or not
+    time: O(nlogn)
+    space: O(1)
+    */
     bool containsDuplicate2(vector<int>& nums) {
-        int n = nums.size();
+        int n = nums.size(); 
 
         sort(nums.begin(), nums.end());
-
+        
         for (int i = 0; i < n-1; ++i) {
             if (nums[i] == nums[i+1]) {
                 return true;
             }
         }
         return false;
-    }
+    }  
 };
 
 int main() {
--- arrays/valid_anagram.cpp
+++ arrays/valid_anagram.cpp
@@ -5,8 +5,14 @@ using namespace std;
 
 class Solution {
 public:
-    bool isAnagram1(string s, string t) {
 
+    /*
+    create 2 unordered maps with the key representing a char and value representing the char's frequency 
+    check if associated values(frequencies) for each key(char) are the same for both maps
+    time: O(n+m)
+    space: O(n+m)
+    */
+    bool isAnagram1(string s, string t) {
         int len = s.length();
 
         if (len != t.length()) return false;
@@ -14,29 +20,34 @@ public:
         unordered_map<char, int> ss, tt;
 
         for (int i = 0; i < len; ++i) {
-            ss[s[i]] += 1;
-            tt[t[i]] += 1;
+            ss[s[i]] += 1;   
+            tt[t[i]] += 1;   // valid for a new element in an unordered set
         }
 
-       if (ss == tt) return true;
+       if (ss == tt) return true;   // operator == for unordered sets cares only about equivalence, not order
        return false;
     }
 
+    /*
+    create a vector of 26 ints, each index representing a char's positon in the alphabetical order
+    for each char, increment or decrement its corresponding position in the vector
+    time: O(n+m)
+    space: O(26)
+    */
     bool isAnagram(string s, string t) {
-
         if(s.size() != t.size()) return false;
 
         vector<int> v(26, 0);
 
-        for(int i = 0; i < s.size(); i++){
-            v[s[i] - 'a']++;
+        for (int i = 0; i < s.size(); ++i) {
+            v[s[i] - 'a']++;   // 'a' - 'a' = 0, 'z' - 'a' = 25 and so on…
         }
 
-        for(int i = 0; i < t.size(); i++){
-            v[t[i] - 'a']--;
+        for (int i = 0; i < t.size(); ++i) {
+            v[t[i] - 'a']--;   
         }
 
-        for(int i = 0; i < 26; i++) if(v[i] != 0) return false;
+        for (int i = 0; i < 26; ++i) if (v[i] != 0) return false;
 
         return true;
     }
