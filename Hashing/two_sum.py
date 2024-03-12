9894a4459f37f1583e5f99e1dbda08fa2885445e
Saboor Bakshi <saboor@Saboors-MacBook.local>
2024-03-11 23:21:15 -0400

    completed two sum

---
+++ Hashing/two_sum.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i != j and nums[i] + nums[j] == target:
+                    return [i, j]
+
+    def twoSumBetter(self, nums, target):
+      hashmap = {}                             # dict basically
+      for i, num in enumerate(nums):
+          complement = target - num
+          if complement in hashmap:
+              return [i, hashmap[complement]]
+          else:
+              hashmap[num] = i 
+
+Solution().twoSum([3, 2, 4], 6)
